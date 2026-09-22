0d117c15435bc110c9e37199b8347751207c6a2b
LawWingLok-2627 <lawwlok2@gmail.com>
2026-09-22 03:14:37 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -4,7 +4,7 @@
 bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you! #tysm :))
 
 # Make me true!
-bool_two = 6767 != 67 or not 
+bool_two = 6767 != 67 or not 1 + 1 < 1 and 
 
 # Make me false!
 bool_three = None
