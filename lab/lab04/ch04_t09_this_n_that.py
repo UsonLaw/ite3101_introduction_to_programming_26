2fa7ead1ddaa8c399d7342475e072c18f84aada1
LawWingLok-2627 <lawwlok2@gmail.com>
2026-09-22 03:09:56 +0000

    [skip actions] lab/lab04/ch04_t09_this_n_that.py

--- lab/lab04/ch04_t09_this_n_that.py
+++ lab/lab04/ch04_t09_this_n_that.py
@@ -4,6 +4,6 @@ bool_two = 1 + 2 == 2 and not 1 > -1 or 1 + 1 < 67
 
 bool_three = 1 + 1 == 2 and not (1>2 or 2<1)
 
-bool_four = not not 60 + 7 == 67
+bool_four = not not 60 + 7 == 67 or 
 
 bool_five = None
